dae1f0ca239a5fabfca3d8d15f7d7ffa731e13ee
Avnish Sharma <avnish.sh7@gmail.com>
2019-11-03 23:41:41 +0530

    Initial commit

---
+++ app/Faculty.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Faculty extends Model
+{
+    //protected $fillable = ['user_id','question','a1','a2','a3','a1hl','a2hl','a3hl','qorder'];
+}
